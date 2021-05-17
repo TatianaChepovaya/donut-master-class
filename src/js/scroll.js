48826e89d4d70558d3a2de6cab1c8b9d632cf8f3
TatianaChepovaya <taniacheport@gmail.com>
2021-05-17 22:41:22 +0300

    плавный скрол

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,4 +1,4 @@
- $("a.scrollto").click(function() {
+ $("a.scroll-to").click(function() {
     var elementClick = $(this).attr("href")
     var destination = $(elementClick).offset().top;
     jQuery("html:not(:animated),body:not(:animated)").animate({
