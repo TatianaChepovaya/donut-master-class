4c2f48a6251ff902134ac6f55745b2a7859ce62b
TatianaChepovaya <taniacheport@gmail.com>
2021-05-17 23:41:01 +0300

    Правки меню

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,4 +1,4 @@
-;(() => {
+; (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]")
   const mobileMenuRef = document.querySelector("[data-menu]")
   menuBtnRef.addEventListener("click", () => {
@@ -7,4 +7,9 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded)
     mobileMenuRef.classList.toggle("is-open")
   })
-})()
+})();
+
+$('.menu__link').on('click', function(){
+  $('.menu__container').removeClass("is-open");
+
+});
